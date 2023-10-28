0dbe40f6c8a2d0145ebf1aae508700a58ffdefa8
llonan <llonan@bk.ru>
2023-10-29 00:35:15 +0300

    initial

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // function onImgBsLoad(){
-document.querySelector('#corpus-a').addEventListener('click', function (event) {
+document.querySelector('#corpus-a').addEventListener('click', function () {
 	open('a.jpg', '_self')
 })
-document.querySelector('#corpus-b').addEventListener('click', function (event) {
+document.querySelector('#corpus-b').addEventListener('click', function () {
 	open('b.jpg', '_self')
 })
-document.querySelector('#corpus-n').addEventListener('click', function (event) {
+document.querySelector('#corpus-n').addEventListener('click', function () {
 	open('n.jpg', '_self')
 })
 // }
